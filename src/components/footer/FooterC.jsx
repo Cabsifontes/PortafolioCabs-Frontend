eade029d021ff490f9f6edaeaf18839577087e42
unknown <sifontes.cabs@gmail.com>
2025-07-19 03:03:29 -0300

    Se realizaron arreglos en footer y ajustes en textcarousel sin afectar funcionalidad

---
+++ src/components/footer/FooterC.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./FooterC.css";
+
+const FooterC = () => {
+  return (
+    <footer className="footer-container">
+      <p className="footer-text">
+        © 2025 Cabs | Todos los derechos reservados.
+      </p>
+    </footer>
+  );
+};
+
+export default FooterC;
